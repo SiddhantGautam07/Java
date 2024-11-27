bdae434820f44072783f8b629f6781eb4ab3dcb2
SiddhantGautam07 <gautam620758@gmail.com>
2024-11-27 19:55:23 +0530

    For loop in Java

---
+++ Loops_03/for_loop_01.java
@@ -0,0 +1,10 @@
+package Loops_03;
+
+public class for_loop_01 {
+    public static void main(String[] args){
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+        System.out.println("Done");
+    }
+}
