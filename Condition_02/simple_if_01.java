2aadc8205c8377b5dbd8f714aabfabfc8d31fd92
SiddhantGautam07 <gautam620758@gmail.com>
2024-11-27 19:53:54 +0530

    Simple If Else Condition

---
+++ Condition_02/simple_if_01.java
@@ -0,0 +1,12 @@
+package Condition_02;
+
+public class simple_if_01 {
+    public static void main(String[] args){
+        int num = 554;
+        if (num % 2 == 0){
+            System.out.println(num + " is Even Number");
+        }else{
+            System.out.println(num + " is Odd Number");;
+        }
+    }
+}
